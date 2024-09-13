53fcbd85098899be6da5cc7c2e51a8067097cf3d
Ericles-Millere <ericlesmiller15@gmail.com>
2024-09-13 01:52:03 -0300

    feat: test to entity transaction reversal

---
+++ src/Domain/Entities/Transactions/TransactionReversal.ts
@@ -0,0 +1,25 @@
+import { Transaction } from "./Transaction";
+import {v4 as uuid} from 'uuid';
+
+export class TransactionReversal {
+  id!: string;
+  transactionId!: string;
+  transaction?: Transaction;
+  reason!: string;
+
+  constructor(id: string | null, transactionId: string, reason: string) {
+    if(id === null)
+      this.id = uuid();
+
+    this.setReason(reason);
+    this.setTransactionId(transactionId);
+  }
+
+  setTransactionId(transactionId : string) : void {
+    this.transactionId = transactionId;
+  }
+
+  setReason(reason: string) : void {
+    this.reason = reason;
+  }
+}
