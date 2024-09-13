72b87792323919119bf7caf560c426a5851e4835
Ericles-Millere <ericlesmiller15@gmail.com>
2024-09-13 01:43:44 -0300

    feat: test to entity userPermission

---
+++ src/Tests/UnitTest/UserPermissions.spec.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import "express-async-errors";
+import { UserPermissions } from "@Domain/Entities/Auth/UserPermissions";
+import {v4 as uuid} from 'uuid';
+
+
+describe('UserPermissions Class', () => {
+  let userPermissions: UserPermissions;
+  let userId = uuid();
+  let permissionId = uuid()
+  beforeEach(() => {
+    userPermissions = new UserPermissions(userId,permissionId,null);
+  });
+
+  test('should create a userPermission with valid properties', () => {
+    expect(userPermissions.permissionId).toBe(permissionId);
+    expect(userPermissions.userId).toBe(userId);
+  });
+
+})
+
+
+
