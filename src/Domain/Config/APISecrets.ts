b020ee3422779082e5e70426e91c5fade1081cdd
Ericles-Millere <ericlesmiller15@gmail.com>
2024-09-15 22:01:27 -0300

    feat: create new loggin to connect database

--- src/Domain/Config/APISecrets.ts
+++ src/Domain/Config/APISecrets.ts
@@ -1,3 +1,6 @@
 export class APISecrets {
   port = Number(process.env.PORT_APP);
+  loggerEnvironment = process.env.LOGGER_ENVIRONMENT;
+  loggerLevel = process.env.LOGGER_LEVEL;
+  loggerServiceName = process.env.LOGGER_SERVICE_NAME;
 }
